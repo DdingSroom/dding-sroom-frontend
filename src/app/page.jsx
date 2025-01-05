451994d26d0e91b561dec49007399d07d5f8a686
YU-JIN-JUNG <yuuuujiiiin@naver.com>
2025-01-05 17:30:46 +0900

    Fix unable to resolve path issue when using alias in import statement

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 'use client';
 
 import React from 'react';
+import Button from '@components/common/Button';
 
 export default function Home() {
   return (
     <>
       <h1>띵스룸</h1>
+      <Button>Test</Button>
     </>
   );
 }
