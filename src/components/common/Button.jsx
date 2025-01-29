ca60411396560e8052c9ca9b7492a806af02e659
YU-JIN-JUNG <yuuuujiiiin@naver.com>
2025-01-29 17:12:53 +0900

    feat: 버튼 컴포넌트 생성

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,5 +1,20 @@
-const Button = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+const Button = ({ onClick, disabled, text, ...props }) => {
+  const buttonText = typeof text === 'string' ? text : '';
+
+  return (
+    <button
+      {...props}
+      className={`flex items-center justify-center w-full h-[2.5625rem] rounded-[0.44963rem] text-[0.97419rem] ${
+        disabled
+          ? 'bg-[#d5d5d5] text-[#9999a2]'
+          : 'bg-[#788cff] text-white cursor-pointer'
+      } `}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {buttonText}
+    </button>
+  );
 };
 
 export default Button;
