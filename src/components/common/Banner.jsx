08ec3312060dd50c46d0a1b0f3ec2778c9d251cf
hello-yujin <yuuuujiiiin@naver.com>
2025-05-21 20:07:44 +0900

    design: 배너 디자인 수정

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -1,21 +1,28 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+
 const Banner = () => {
   return (
     <div className="flex justify-between items-center p-7 bg-white rounded-2xl w-full max-w-[95%] h-auto min-h-[15rem]">
-      <div className="flex flex-col gap-2.5 ">
+      <div className="flex flex-col gap-2.5">
         <div>
           <div>오늘의 혼잡도</div>
-          <div className="text-5xl text-[#788DFF] ">여유로움</div>
+          <div className="text-5xl text-[#788DFF]">여유로움</div>
         </div>
         <div>
           <div>로그인하여 자리를 예약해보세요!</div>
-          <button className="flex ">
-            로그인{' '}
-            <img
-              src="/static/icons/arrow_icon.png"
-              alt="arrow"
-              className="gap-[10px] "
-            />
-          </button>
+          <Link href="/login">
+            <button className="flex items-center gap-2 mt-2">
+              로그인
+              <img
+                src="/static/icons/arrow_icon.png"
+                alt="arrow"
+                className="w-4 h-4"
+              />
+            </button>
+          </Link>
         </div>
       </div>
       <img
