67a69fb8fed340441f3c96849ecb7e348f904ccb
hello-yujin <yuuuujiiiin@naver.com>
2025-07-09 11:21:25 +0900

    feat: accessToken 기준 배너 렌더링

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import Header from '@components/common/Header';
+import Banner from '@components/common/Banner';
 import AfterLoginBanner from '@components/common/AfterLoginBanner';
 import SecondBanner from '@components/common/SecondBanner';
 import ReservationSection from '@components/common/ReservationSection';
@@ -18,17 +19,14 @@ export default function Home() {
       try {
         const decoded = jwtDecode(accessToken);
         const { id, email } = decoded;
-
         setUserInfo({ id, email });
         console.log('디코드된 사용자 정보:', decoded);
       } catch (e) {
         console.error('토큰 디코딩 실패:', e);
         alert('토큰이 유효하지 않습니다. 다시 로그인해주세요.');
-        // window.location.href = '/';
       }
     } else {
       console.warn('로그인하지 않은 상태입니다.');
-      // window.location.href = '/';
     }
   }, [accessToken]);
 
@@ -38,7 +36,11 @@ export default function Home() {
         <Header />
       </div>
       <div className="flex justify-center w-full">
-        <AfterLoginBanner className="w-full" />
+        {accessToken ? (
+          <AfterLoginBanner className="w-full" />
+        ) : (
+          <Banner className="w-full" />
+        )}
       </div>
       <div className="flex justify-center w-full">
         <SecondBanner className="w-full" />
