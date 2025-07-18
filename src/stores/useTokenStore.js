3e22cf8bf2fb2f1fb7ff21884765354ff198235b
hello-yujin <yuuuujiiiin@naver.com>
2025-07-19 01:53:23 +0900

    Feat: userId 상태 및 setUserId 액션 추가

--- src/stores/useTokenStore.js
+++ src/stores/useTokenStore.js
@@ -3,8 +3,10 @@ import { create } from 'zustand';
 const useTokenStore = create((set) => ({
   accessToken: '',
   refreshToken: '',
+  userId: null,
   setAccessToken: (token) => set({ accessToken: token }),
   setRefreshToken: (token) => set({ refreshToken: token }),
+  setUserId: (id) => set({ userId: id }),
 }));
 
 export default useTokenStore;
