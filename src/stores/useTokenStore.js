b2819ac9b5654358152b8a8987623438845ff835
hello-yujin <yuuuujiiiin@naver.com>
2025-07-31 17:46:50 +0900

    Feat: useTokenStore에 sessionStorage 연동 및 토큰 초기화 함수 추가

--- src/stores/useTokenStore.js
+++ src/stores/useTokenStore.js
@@ -1,12 +1,48 @@
 import { create } from 'zustand';
 
 const useTokenStore = create((set) => ({
-  accessToken: '',
-  refreshToken: '',
-  userId: null,
-  setAccessToken: (token) => set({ accessToken: token }),
-  setRefreshToken: (token) => set({ refreshToken: token }),
-  setUserId: (id) => set({ userId: id }),
+  accessToken:
+    typeof window !== 'undefined'
+      ? sessionStorage.getItem('accessToken') || ''
+      : '',
+  refreshToken:
+    typeof window !== 'undefined'
+      ? sessionStorage.getItem('refreshToken') || ''
+      : '',
+  userId:
+    typeof window !== 'undefined'
+      ? parseInt(sessionStorage.getItem('userId')) || null
+      : null,
+
+  setAccessToken: (token) => {
+    set({ accessToken: token });
+    if (typeof window !== 'undefined') {
+      sessionStorage.setItem('accessToken', token);
+    }
+  },
+
+  setRefreshToken: (token) => {
+    set({ refreshToken: token });
+    if (typeof window !== 'undefined') {
+      sessionStorage.setItem('refreshToken', token);
+    }
+  },
+
+  setUserId: (id) => {
+    set({ userId: id });
+    if (typeof window !== 'undefined') {
+      sessionStorage.setItem('userId', id.toString());
+    }
+  },
+
+  clearTokens: () => {
+    set({ accessToken: '', refreshToken: '', userId: null });
+    if (typeof window !== 'undefined') {
+      sessionStorage.removeItem('accessToken');
+      sessionStorage.removeItem('refreshToken');
+      sessionStorage.removeItem('userId');
+    }
+  },
 }));
 
 export default useTokenStore;
