d8be398f2da5d9c87379e96963fa8b6a1946f29b
hello-yujin <yuuuujiiiin@naver.com>
2025-08-04 19:42:54 +0900

    Style: 예약 모달 UI 개선 및 버튼 시각적 레이아웃 정리

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,28 +1,30 @@
-const Modal = ({ isOpen, onClose, onSubmit, children, text, color }) => {
+const Modal = ({ isOpen, onClose, onSubmit, children, text }) => {
   if (!isOpen) return null;
-
-  const colorClass = color === 'red' ? 'text-red-500' : 'text-gray-500';
 
   return (
     <div
-      className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center inset-0 z-[9999]"
+      className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-[9999]"
       onClick={onClose}
     >
       <div
-        className="bg-white rounded-lg shadow-lg p-4 relative w-[90%] max-w-2xl h-[40%] flex flex-col"
+        className="bg-white rounded-[20px] w-[90%] max-w-[500px] p-6 sm:p-8 flex flex-col justify-between"
         onClick={(e) => e.stopPropagation()}
       >
-        {children}
-        <div className="flex justify-center justify-between p-2 cursor-pointer">
-          <button className="text-gray-500 w-[50%]" onClick={onClose}>
+        <div className="overflow-y-auto max-h-[70vh]">{children}</div>
+
+        <div className="flex w-full mt-6 rounded-xl overflow-hidden border border-[#3250F5]">
+          <button
+            onClick={onClose}
+            className="w-1/2 py-3 bg-white text-[#5C5C5C] text-sm font-medium rounded-none"
+          >
             취소
           </button>
           <button
-            className={`text-gray-500 w-[50%] ${colorClass}`}
             onClick={(e) => {
               e.stopPropagation();
               if (onSubmit) onSubmit();
             }}
+            className="w-1/2 py-3 bg-[#7389FF] text-white text-sm font-semibold rounded-none"
           >
             {text}
           </button>
