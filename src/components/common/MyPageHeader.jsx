1b38c9b47149bfa007bc11de219411271bf1cdc1
hello-yujin <yuuuujiiiin@naver.com>
2025-08-05 16:41:57 +0900

    Fix: 마이페이지 상단 화살표 클릭 시 홈으로 이동되도록 수정

--- src/components/common/MyPageHeader.jsx
+++ src/components/common/MyPageHeader.jsx
@@ -1,15 +1,20 @@
-import Link from 'next/link';
+'use client';
+
+import { useRouter } from 'next/navigation';
 
 const MyPageHeader = () => {
+  const router = useRouter();
+
   return (
     <header className="flex items-center p-4 pt-8 pb-12 bg-[#FFFF] relative">
-      <Link href="/my/mypage">
+      <button onClick={() => router.push('/')}>
         <img
           src="/static/icons/arrow_left_icon.svg"
           alt="arrow"
           className="h-8"
         />
-      </Link>
+      </button>
+
       <div className="absolute left-1/2 transform -translate-x-1/2 text-2xl">
         마이 페이지
       </div>
