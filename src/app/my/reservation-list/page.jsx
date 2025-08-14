40eb4f503edff68e0bf081843f18b1c38750e971
hello-yujin <yuuuujiiiin@naver.com>
2025-08-14 20:55:17 +0900

    Feat: 예약 목록 페이지에 PrivacyPolicyFooter 추가 및 레이아웃 통일

--- src/app/my/reservation-list/page.jsx
+++ src/app/my/reservation-list/page.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import MyPageHeader from '@components/common/MyPageHeader';
 import ReservationList from '@components/common/ReservationList';
 import LoginRequiredModal from '@components/common/LoginRequiredModal';
+import PrivacyPolicyFooter from '@components/common/PrivacyPolicyFooter';
 import useTokenStore from '../../../stores/useTokenStore';
 
 export default function ReservationInfo() {
@@ -22,14 +23,18 @@ export default function ReservationInfo() {
   };
 
   return (
-    <div className="w-full">
-      <MyPageHeader />
-      {!showLoginModal && <ReservationList />}
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <main className="flex-1">
+        <MyPageHeader />
+        {!showLoginModal && <ReservationList />}
+      </main>
 
       <LoginRequiredModal
         isOpen={showLoginModal}
         onConfirm={handleLoginConfirm}
       />
+
+      <PrivacyPolicyFooter />
     </div>
   );
 }
