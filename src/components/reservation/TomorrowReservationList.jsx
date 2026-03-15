b6dcf74cff6f0f48e726767c291e460eba3df3c7
hello-yujin <yuuuujiiiin@naver.com>
2026-03-15 16:17:11 +0900

    refactor: 내일예약에서 rooms 상수를 constants로 분리

--- src/components/reservation/TomorrowReservationList.jsx
+++ src/components/reservation/TomorrowReservationList.jsx
@@ -1,25 +1,10 @@
 import TomorrowReservationComponent from '@components/reservation/TomorrowReservationComponent';
+import { ROOMS } from '@constants/rooms';
 
 const TomorrowReservationList = () => {
-  const rooms = [
-    { id: 1, name: '스터디룸 1', caption: '5인실' },
-    { id: 2, name: '스터디룸 2', caption: '5인실' },
-    { id: 3, name: '스터디룸 3', caption: '5인실' },
-    { id: 4, name: '스터디룸 4', caption: '5인실' },
-    { id: 5, name: '스터디룸 5', caption: '5인실' },
-    { id: 6, name: '휴게공간 1' },
-    { id: 7, name: '휴게공간 2' },
-    { id: 8, name: '휴게공간 3' },
-    {
-      id: 9,
-      name: '휴게공간 4',
-      notice: '장애우 전용 공간이므로\n장애우 분들만 예약 가능합니다.',
-    },
-  ];
-
   return (
     <div className="w-full">
-      {rooms.map((room, index) => (
+      {ROOMS.map((room, index) => (
         <TomorrowReservationComponent
           key={room.id}
           index={index + 1}
