c6617f1c65dad1d7a075bd9c6b6cf0f693623bfb
hello-yujin <yuuuujiiiin@naver.com>
2026-03-15 16:16:17 +0900

    refactor: 예약 공간 목록을 constants/rooms.js로 분리

---
+++ src/constants/rooms.js
@@ -0,0 +1,15 @@
+export const ROOMS = [
+  { id: 1, name: '스터디룸 1', caption: '5인실' },
+  { id: 2, name: '스터디룸 2', caption: '5인실' },
+  { id: 3, name: '스터디룸 3', caption: '5인실' },
+  { id: 4, name: '스터디룸 4', caption: '5인실' },
+  { id: 5, name: '스터디룸 5', caption: '5인실' },
+  { id: 6, name: '휴게공간 1' },
+  { id: 7, name: '휴게공간 2' },
+  { id: 8, name: '휴게공간 3' },
+  {
+    id: 9,
+    name: '휴게공간 4',
+    notice: '장애우 전용 공간이므로\n장애우 분들만 예약 가능합니다.',
+  },
+];
