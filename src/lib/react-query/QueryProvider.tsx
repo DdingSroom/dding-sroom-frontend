9f552e7d3ac8348cb5c23c7f8037377cc6e417bf
hello-yujin <yuuuujiiiin@naver.com>
2026-03-30 18:21:51 +0900

    refactor: QueryProvider props 타입을 interface로 분리하여 컨벤션 반영

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -4,6 +4,10 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import dynamic from 'next/dynamic';
 import { getQueryClient } from './queryClient';
 
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+
 const ReactQueryDevtools = dynamic(
   () =>
     import('@tanstack/react-query-devtools').then(
@@ -12,11 +16,7 @@ const ReactQueryDevtools = dynamic(
   { ssr: false },
 );
 
-export default function QueryProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function QueryProvider({ children }: QueryProviderProps) {
   const queryClient = getQueryClient();
 
   return (
