a63a106ca3817199d4530ef9bf4c07699b401a44
jogpfls <joerin4177@gmail.com>
2026-03-31 00:19:00 +0900

    fix: next-env.d.ts를 eslint ignores 목록에 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const config = [
   {
-    ignores: ['node_modules/', '.next/'],
+    ignores: ['node_modules/', '.next/', 'next-env.d.ts'],
   },
 
   ...compat.extends('next/core-web-vitals'),
