8d62485abf12699cc7e542bddf204c7148c56d14
hello-yujin <yuuuujiiiin@naver.com>
2026-03-30 18:16:42 +0900

    style: queryClient defaultOptions 내 불필요한 공백 제거

--- src/lib/react-query/queryClient.ts
+++ src/lib/react-query/queryClient.ts
@@ -17,11 +17,8 @@ function makeQueryClient() {
     defaultOptions: {
       queries: {
         staleTime: 60 * 1000,
-
         gcTime: 5 * 60 * 1000,
-
         retry: 1,
-
         refetchOnWindowFocus: false,
       },
       mutations: {
