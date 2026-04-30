2dba37f8d61d6b3c106169232b7a9a73da37e0ee
odukong <ohbin1017@gmail.com>
2026-04-30 15:14:30 +0900

    feat: icon폴더 파일 진입점 추가
    - public/icon 폴더에 접근하기 위한 진입점이 되는 index.ts 파일을 추가하였습니다.
    - public/icon/index.ts 파일의 prettier 적용을 위해 prettierignore에서 public을 제거하였습니다.
    - 색상만 다른 단순 아이콘(하단 메뉴바 아이콘)은 하나의 아이콘을 재활용하여 사용할 수 있도록 svg파일 속성을 변경하였습니다. (메뉴바 아이콘 중복 파일 삭제/수정에 따른 FooterNav파일을 수정하였습니다.
    - 그 외 svg 아이콘은 공통으로 사용하기에 속성을 제어하기 어렵다고 판단하여 다른 svg로 활용할 수 있도록 유지하였습니다.

---
+++ public/static/icons/index.ts
@@ -0,0 +1,19 @@
+export { default as ArrowLeft } from './arrow_left_icon.svg';
+export { default as ArrowRight } from './arrow_right_icon.svg';
+
+// 하단 메뉴바 아이콘
+export { default as Reservation } from './reservation_icon.svg';
+export { default as Community } from './community_icon.svg';
+export { default as Proposal } from './proposal_icon.svg';
+
+export { default as CheckCircle } from './check_circle_icon.svg';
+export { default as CheckOff } from './check_off_icon.svg';
+export { default as CheckOn } from './check_on_icon.svg';
+
+export { default as EyeOn } from './eye_on_icon.svg';
+export { default as EyeOff } from './eye_off_icon.svg';
+
+export { default as Logo } from './logo.svg';
+export { default as MaruSad } from './maru_sad_icon.svg';
+
+export { default as X } from './x_icon.svg';
