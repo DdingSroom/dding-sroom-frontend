5fd3f8ff5e96d262583595944a85b3ba2a5d73d9
hello-yujin <yuuuujiiiin@naver.com>
2026-05-03 21:45:08 +0900

    feat: withSentryConfig 래핑 및 Source Map 업로드 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from 'next';
 import path from 'path';
 
+import { withSentryConfig } from '@sentry/nextjs';
+
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
@@ -35,4 +37,17 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: true,
+
+  sourcemaps: {
+    deleteSourcemapsAfterUpload: true,
+  },
+
+  telemetry: false,
+});
