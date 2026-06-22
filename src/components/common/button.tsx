92b88ec0a7e95c717ffa146574501d419795dcda
Sohyunnnn <thgus05061@naver.com>
2026-06-22 17:49:50 +0900

    refactor: 컬러 스타일 수정

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -14,7 +14,7 @@ const Button = ({ onClick, children, disabled, ...props }: ButtonProps) => (
     className={`flex justify-center text-white w-full py-3 rounded-lg text-body-01 font-medium transition-all duration-200 ${
       disabled
         ? 'bg-gray-200 cursor-not-allowed'
-        : 'bg-brand cursor-pointer hover:bg-brand-hover active:bg-brand-active hover:shadow-md'
+        : 'bg-primary-400 cursor-pointer hover:bg-primary-500 active:bg-primary-600 hover:shadow-md'
     }`}
     onClick={onClick}
   >
