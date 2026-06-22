1bf2ab95e3a48975b9589b32422a42b8482eab69
Sohyunnnn <thgus05061@naver.com>
2026-06-22 17:58:42 +0900

    refactor: 공통 Button의 기본 HTML 속성 지원

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,22 +1,23 @@
-import type { ReactNode } from 'react';
+import type { ButtonHTMLAttributes } from 'react';
 
-interface ButtonProps {
-  onClick: () => void;
-  children: ReactNode;
-  disabled?: boolean;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
 }
-
-const Button = ({ onClick, children, disabled, ...props }: ButtonProps) => (
+const Button = ({
+  type = 'button',
+  children,
+  disabled,
+  ...props
+}: ButtonProps) => (
   <button
     disabled={disabled}
-    type="button"
+    type={type}
     {...props}
     className={`flex justify-center text-white w-full py-3 rounded-lg text-body-01 font-medium transition-all duration-200 ${
       disabled
         ? 'bg-gray-200 cursor-not-allowed'
         : 'bg-primary-400 cursor-pointer hover:bg-primary-500 active:bg-primary-600 hover:shadow-md'
     }`}
-    onClick={onClick}
   >
     {children}
   </button>
