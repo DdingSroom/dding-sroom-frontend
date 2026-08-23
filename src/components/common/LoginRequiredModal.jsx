e0afc527d2a7ace0633701f53e82e1745747fef6
u-zzn <yuuuujiiiin@naver.com>
2026-08-23 19:00:55 +0900

    refactor: LoginRequiredModal 교체 후 마지막 사용했던 AlertModal 제거

--- src/components/common/LoginRequiredModal.jsx
+++ src/components/common/LoginRequiredModal.jsx
@@ -1,14 +1,16 @@
 'use client';
 
-import AlertModal from './AlertModal';
+import BasicModal from './BasicModal';
 
 const LoginRequiredModal = ({ isOpen, onConfirm }) => (
-  <AlertModal
+  <BasicModal
     isOpen={isOpen}
     onClose={onConfirm}
     closeOnOverlayClick={false}
+    className="max-w-modal-sm"
     title="로그인이 필요한 기능입니다"
     message="이 페이지를 이용하려면 로그인이 필요합니다."
+    actions={[{ text: '확인', onClick: onConfirm }]}
   />
 );
 
